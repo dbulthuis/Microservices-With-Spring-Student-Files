3cb1a33c5c0ca98a2337d75ec7bef3de4b0c48a0
dbulthuis <dbulthuis@gmail.com>
2017-11-09 15:03:45 -0600

    Finished lab 7

---
+++ myWork/lab-6/sentence-server/src/main/java/demo/dao/NounClient.java
@@ -0,0 +1,13 @@
+package demo.dao;
+
+import demo.domain.Word;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("NOUN")
+public interface NounClient {
+    @GetMapping("/")
+    public Word getWord();
+}
